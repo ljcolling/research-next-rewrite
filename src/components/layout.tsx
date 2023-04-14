c9b54b7669418cc1210de471378e7112ce121c82
Lincoln Colling <lincoln@colling.net.nz>
2023-04-14 12:01:17 +0100

    fixed mobile menu

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head'
-import Link from "next/link";
+import NextLink from "next/link";
 
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useState, type ReactNode, type Dispatch, type SetStateAction } from "react"
+import { useRouter } from 'next/router';
 
 
 const Main = ({ children }: { children: ReactNode }) => {
@@ -15,63 +17,13 @@ const Main = ({ children }: { children: ReactNode }) => {
 
 
 const menuitems: Array<{ text: string, link: string }> = [
-  { text: "Research", link: "research" },
-  { text: "Publications", link: "publications" },
-  { text: "Teaching", link: "teaching" },
+  { text: "Home", link: "/" },
+  { text: "Research", link: "/research" },
+  { text: "Publications", link: "/publications" },
+  { text: "Teaching", link: "/teaching" },
   { text: "Blog", link: "http://blog.colling.net.nz" },
 ]
 
-
-
-const Menu = ({ items }: {
-  items: typeof menuitems
-}) => {
-
-  return (
-    <div className="md:static md:min-h-fit md:w-auto">
-      <ul className="flex md:flex-row flex-col md:items-center md:gap-8 md:p-0 md:mx-auto " >
-        <li className="md:p-0 py-2">
-          <Link href="./" className="font-semibold">Home</Link>
-        </li>
-        {items.map((value, key) => {
-          return (<li key={key} className="md:p-0 pt-2 ">
-            <Link className="" key={key} href={value.link}>{value.text}</Link>
-          </li>)
-        })
-        }
-      </ul>
-    </div>
-
-  )
-
-
-}
-
-const Navbar = () => {
-  const [display, setDisplay] = useState<boolean>(false);
-
-  return (
-    <header className="shadow p-5">
-      <nav className=''>
-        <div className='flex md:flex-row flex-col mx-auto max-auto md:justify-between justify-center'>
-          <div className='flex-col'>
-          <div className='flex flex-row pb-5 md:pb-0'>
-          <div className="md:block md:relative absolute">Lincoln <strong>Colling</strong></div>
-          <button className="md:hidden hover:text-blue-500 absolute md:relative right-5" role='button' onClick={() => setDisplay(!display)} >=</button>
-          </div>
-          </div>
-        <div>
-            { display ? null :
-          <div className='pt-2 md:pt-0'>
-            <Menu items={menuitems} />
-          </div>
-              }
-          </div>
-        </div>
-      </nav>
-    </header>
-  )
-}
 
 const Body = ({ children }: { children: ReactNode }) => {
   return (
@@ -108,7 +60,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <Body>
-      <Navbar />
+      <NavBar />
       <Main>
         {children}
       </Main>
@@ -116,4 +68,130 @@ export default function Layout({ children }: { children: ReactNode }) {
     </Body>
   </>
   )
-} 
+}
+
+
+
+
+
+
+
+
+const MobileNav = ({
+  open,
+  setOpen,
+  items,
+}: {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  items: typeof menuitems
+}) => {
+
+
+  return (
+    <div
+      style={{height: "100%"}}
+      className={`absolute top-0 left-0  h-full w-screen z-10 bg-gray-800 transform 
+${open ? "-translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out filter drop-shadow-md 
+
+`}
+    >
+      <div className="flex flex-col mt-9 ml-4">
+        {
+          items.map((value, key) => {
+            return (
+              <NextLink
+                key={key}
+                href={value.link}
+                className={`text-slate-100
+text-xl font-normal my-4 
+
+                  } ${value.text === "Home" ? "font-semibold text-white" : ""}`}
+                onClick={() => setOpen(!open)}
+              >
+                {value.text}
+              </NextLink>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+};
+
+export const NavBar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className={`flex md:flex-row px-4 py-4 h-20 max-auto items-center
+${open ? "overflow-none z-10" : ""}
+      `}>
+        <NextLink href="/">
+      <div className="flex md:flex-row flex-col h-12 w-auto items-center md:space-x-1">
+          <span>Lincoln</span>
+        <span><strong>Colling</strong></span>
+      </div>
+      </NextLink>
+      <MobileNav open={open} setOpen={setOpen} items={menuitems} />
+      <div className="w-full flex justify-end ">
+        <div
+          className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          {/* hamburger button */}
+          <span
+            className={`h-1 w-full bg-slate-300 rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""
+              }`}
+          />
+          <span
+            className={`h-1 w-full bg-slate-300 rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"
+              }`}
+          />
+          <span
+            className={`h-1 w-full bg-slate-300 rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""
+              }`}
+          />
+        </div>
+
+        <div className="hidden md:flex">
+          {
+            menuitems.map((value, key) => {
+              return (
+                <NavLink key={key} to={value.link}>{value.text}</NavLink>
+              )
+            })
+          }
+        </div>
+      </div>
+    </nav>
+
+  );
+};
+
+const NavLink = ({
+  to,
+  children,
+}: {
+  to: string;
+  children: React.ReactNode;
+}) => {
+  const router = useRouter();
+
+  return (
+    <a
+      href={to}
+      className={`px-4 transition ease-in-out duration-300 relative 
+      ${router.asPath === to
+        ? "text-purple-500"
+        : "stroke  hover:text-purple-300"
+        }`}
+    >
+      {children}
+    </a>
+  );
+};
+
+
+
