98da74f835659fc9be01142782b9ad079f758d88
Lincoln Colling <lincoln@colling.net.nz>
2023-04-17 09:25:55 +0100

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
+import { Analytics } from '@vercel/analytics/react'
+;
+
 import "~/styles/globals.css";
 
 import Layout from "../components/layout"
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return (<Layout><Component {...pageProps} /></Layout>);
+  return (<>
+    <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  <Analytics />
+  </>
+  );
 };
 
 export default MyApp;
